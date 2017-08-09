d7a8ffb6e78f4596a17bc2990a92ec7c8e019030
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-08-09 16:41:25 +0700

    add class and provider for client service

---
+++ app/Client/Client.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace App\Client;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+
+class Client
+{
+    /**
+     * The GuzzleHttp Client instance.
+     *
+     * @var \GuzzleHttp\Client
+     */
+    protected $http;
+
+    /**
+     * The Endpoint instance.
+     *
+     * @var string
+     */
+    protected $endpoint;
+
+    /**
+     * The headers that will be sent when call the API.
+     *
+     * @var array
+     */
+    protected $headers = [];
+
+    /**
+     * The body that will be sent when call the API.
+     *
+     * @var mixed
+     */
+    protected $body;
+
+    /**
+     * The query that will be sent when call the API.
+     *
+     * @var array
+     */
+    protected $query = [];
+
+     /**
+     * Create a new Class instance.
+     *
+     * @param  \GuzzleHttp\Client  $http
+     * @return void
+     */
+    public function __construct(HttpClient $http)
+    {
+        $this->http = $http;
+
+        $this->headers = $this->headers();
+    }
+
+    /**
+     * The headers that will be sent when call the API.
+     *
+     * @var array
+     */
+    public function headers()
+    {
+        return $this->headers = [
+            // 'client_key'   => config('restapi.key'),
+        ];
+    }
+
+    /**
+     * The headers that will be sent when call the API.
+     *
+     * @var array
+     */
+    public function uri()
+    {
+        return config('restapi.uri').$this->endpoint;
+    }
+
+    /**
+     * Set request endpoint.
+     *
+     * @param  \GuzzleHttp\Client  $http
+     * @return App\RestMiddleware\Client
+     */
+    public function setEndpoint($endpoint = '')
+    {
+        $this->endpoint = $endpoint;
+
+        return $this;
+    }
+
+    /**
+     * Set header for request.
+     *
+     * @param  array  $headers
+     * @return App\RestMiddleware\Client
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = array_merge($this->headers(), $headers);
+
+        return $this;
+    }
+
+    /**
+     * Set body for request.
+     *
+     * @param  mixed  $body
+     * @return App\RestMiddleware\Client
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Set body for request.
+     *
+     * @param  array  $query
+     * @return App\RestMiddleware\Client
+     */
+    public function setQuery(array $query)
+    {
+        $this->query = http_build_query($query);
+
+        return $this;
+    }
+
+    /**
+     * Get request from middleware.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get()
+    {
+        try {
+            $request  = $this->http->request('GET', $this->uri(), [
+                'headers'  => $this->headers,
+                'query'    => $this->query
+            ]);
+            $response = json_decode($request->getBody(), true);
+        } catch (ClientException $e) {
+            $body = $e->getResponse()->getBody();
+            $response = json_decode($body->getContents(), true);
+        } catch (ServerException $e) {
+            abort(500);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Post request to middleware.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function post($type = 'json')
+    {
+        try {
+            $request  = $this->http->request('POST', $this->uri(), [
+                'headers'  => $this->headers,
+                'query'    => $this->query,
+                $type      => $this->body
+            ]);
+            $response = json_decode($request->getBody(), true);
+        } catch (ClientException $e) {
+            $body = $e->getResponse()->getBody();
+            $response = json_decode($body->getContents(), true);
+        } catch (ServerException $e) {
+            \Log::info($e->getRequest()->getBody());
+            abort(500);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Post request to middleware.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function put()
+    {
+        try {
+            $request  = $this->http->request('PUT', $this->uri(), [
+                'headers'  => $this->headers,
+                'query'    => $this->query,
+                'json'     => $this->body
+            ]);
+            $response = json_decode($request->getBody(), true);
+        } catch (ClientException $e) {
+            $body = $e->getResponse()->getBody();
+            $response = json_decode($body->getContents(), true);
+        } catch (ServerException $e) {
+            abort(500);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Delete request to middleware.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function deleted()
+    {
+        try {
+            $request  = $this->http->request('DELETE', $this->uri(), [
+                'headers'  => $this->headers,
+                'query'    => $this->query,
+                'json'     => $this->body
+            ]);
+            $response = json_decode($request->getBody(), true);
+        } catch (ClientException $e) {
+            $body = $e->getResponse()->getBody();
+            $response = json_decode($body->getContents(), true);
+        } catch (ServerException $e) {
+            abort(500);
+        }
+
+        return $response;
+    }
+}
---
+++ config/restapi.php
@@ -0,0 +1,7 @@
+<?php
+
+	return [
+		'uri' => env('CLIENT_URI', 'http://private-134481-mortagebri.apiary-mock.com/api/v1/int/'),
+	  	'key' => env('CLIENT_KEY', '$2y$10$OoDAS6saH1b3D/nZJ4DXKuOTqVumFTACUZDFkZfepS1h15jDNxdzK'),
+	]
+?>
