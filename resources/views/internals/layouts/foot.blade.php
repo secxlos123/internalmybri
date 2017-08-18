bbd48faebe312a668c20cda2f4fd7f2f0a24a441
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-08-18 14:07:36 +0700

    layout and foot modify

--- resources/views/internals/layouts/foot.blade.php
+++ resources/views/internals/layouts/foot.blade.php
@@ -36,6 +36,8 @@
         <script src="{{asset('assets/js/jquery.wizard-init.js')}}" type="text/javascript"></script>
         <script src="{{asset('assets/js/jquery.date-pickers.init.js')}}"></script>
         <script src="{{asset('assets/js/jquery.time-pickers.init.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.gmaps.js')}}"></script>
+
 
         <script>
            $(document).ready(function() {
@@ -55,6 +57,28 @@
                    e.preventDefault();
                });
            });
+
+           $(".numericOnly").keydown(function (e) {
+            // Allow: backspace, delete, tab, escape, enter and .
+            if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                 // Allow: Ctrl+A
+                (e.keyCode == 65 && e.ctrlKey === true) ||
+                 // Allow: Ctrl+C
+                (e.keyCode == 67 && e.ctrlKey === true) ||
+                 // Allow: Ctrl+X
+                (e.keyCode == 88 && e.ctrlKey === true) ||
+                // Allow: backspace
+                (e.keyCode === 320 && e.ctrlKey === true) ||
+                 // Allow: home, end, left, right
+                (e.keyCode >= 35 && e.keyCode <= 39)) {
+                     // let it happen, don't do anything
+                     return;
+            }
+            // Ensure that it is a number and stop the keypress
+              if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                    e.preventDefault();
+            }
+        });
         </script>
 @include('internals.layouts.signout')
     </body>
