da8adfa5126f3ade06492cb872762df6e1d52cc8
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-09-05 16:37:44 +0700

    modify developer detail

--- resources/views/internals/developers/detail.blade.php
+++ resources/views/internals/developers/detail.blade.php
@@ -80,6 +80,12 @@
                                                                             <p class="form-control-static">{{$dataDev['city_name']}}</p>
                                                                         </div>
                                                                     </div>
+                                                                    <div class="form-group">
+                                                                        <label class="col-md-4 control-label">Plafon :</label>
+                                                                        <div class="col-md-8">
+                                                                            <p class="form-control-static">{{$dataDev['plafond']}}</p>
+                                                                        </div>
+                                                                    </div>
                                                                 </form>
                                                             </div>
                                                             <div class="col-md-6">
@@ -94,6 +100,18 @@
                                                                         <label class="col-md-4 control-label">No. Telepon :</label>
                                                                         <div class="col-md-8">
                                                                             <p class="form-control-static">{{$dataDev['phone']}}</p>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group">
+                                                                        <label class="col-md-4 control-label">Ringkasan :</label>
+                                                                        <div class="col-md-8">
+                                                                            <p class="form-control-static">{{$dataDev['summary']}}</p>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group">
+                                                                        <label class="col-md-4 control-label">Nomor PKS :</label>
+                                                                        <div class="col-md-8">
+                                                                            <p class="form-control-static">{{$dataDev['pks_number']}}</p>
                                                                         </div>
                                                                     </div>
                                                                 </form>
