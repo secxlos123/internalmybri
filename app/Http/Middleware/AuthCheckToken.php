69d42653dab2afe296c58a13affb462c35ad4f57
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-09-07 19:42:00 +0700

    add middleware to check token

---
+++ app/Http/Middleware/AuthCheckToken.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthCheckToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $checkToken = \Client::setEndpoint('check-token')
+                      ->setHeaders(['Authorization' => session()->get('user.contents.token'),
+                                    'pn' => session()->get('user.contents.pn')
+                      ])->get();
+        // dd($checkToken);
+
+        if($checkToken['contents']['refreshed'] == true){
+            session()->put('user.contents.token', $checkToken['contents']['token']);
+        }else{
+            return $next($request);
+        }
+
+        return $next($request);
+    }
+}
