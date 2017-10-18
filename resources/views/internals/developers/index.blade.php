3cc22cbc07cbaf3816d7916e45962c215632bddd
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-10-18 13:15:41 +0700

    modifying developer index to be filtering first

--- resources/views/internals/developers/index.blade.php
+++ resources/views/internals/developers/index.blade.php
@@ -29,10 +29,10 @@
                 <div class="col-sm-12">
                     <div class="card-box table-responsive">
                         <div class="add-button">
-                            <a href="#filter" class="btn btn-primary waves-light waves-effect w-md m-b-15" data-toggle="collapse"><i class="mdi mdi-filter"></i> Filter</a>
+                            <!-- <a href="#filter" class="btn btn-primary waves-light waves-effect w-md m-b-15" data-toggle="collapse"><i class="mdi mdi-filter"></i> Filter</a> -->
                             <a href="{{route('developers.create')}}" class="btn btn-primary waves-light waves-effect w-md m-b-15"><i class="mdi mdi-plus-circle-outline"></i> Tambah Developer</a>
                         </div>
-                        <div id="filter" class="collapse m-b-15">
+                        <div id="filter" class="m-b-15">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="card-box">
@@ -110,16 +110,21 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 
 <script type="text/javascript">
-    var resizefunc = [];
-    $(document).ready(function () {
-        var lastStatusElement = null;
-        $('.select2').select2({
-            witdh : '100%',
-            allowClear: true,
+    var table1 = $('#datatable').DataTable({
+            searching: false,
+            "language": {
+                "emptyTable": "No data available in table"
+            }
         });
-        var baseurl = window.location.origin;
 
-        var table = $('#datatable').dataTable({
+    $(document).on('click', "#btn-filter", function(){
+        table1.destroy();
+        reloadData1($('.cities').val(), $('.projects').val());
+      })
+
+    function reloadData1(city_id, project)
+      {
+        table1 = $('#datatable').DataTable({
             processing : true,
             serverSide : true,
             lengthMenu: [
@@ -130,7 +135,7 @@
                 infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
             },
             ajax : {
-                url : baseurl+'/datatables/developers',
+                url : '/datatables/developers',
                 data : function(d, settings){
                     var api = new $.fn.dataTable.Api(settings);
 
@@ -165,10 +170,66 @@
                 { data: 'action', name: 'action', bSortable: false },
             ],
         });
-
-        $('#btn-filter').on('click', function () {
-            table.fnDraw();
+      }
+    var resizefunc = [];
+    $(document).ready(function () {
+        var lastStatusElement = null;
+        $('.select2').select2({
+            witdh : '100%',
+            allowClear: true,
         });
+        // var baseurl = window.location.origin;
+
+        // var table = $('#datatable').dataTable({
+        //     processing : true,
+        //     serverSide : true,
+        //     lengthMenu: [
+        //         [ 10, 25, 50, -1 ],
+        //         [ '10', '25', '50', 'All' ]
+        //     ],
+        //     language : {
+        //         infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
+        //     },
+        //     ajax : {
+        //         url : baseurl+'/datatables/developers',
+        //         data : function(d, settings){
+        //             var api = new $.fn.dataTable.Api(settings);
+
+        //             d.page = Math.min(
+        //                 Math.max(0, Math.round(d.start / api.page.len())),
+        //                 api.page.info().pages
+        //             );
+
+        //             d.city_id = $('.cities').val();
+        //             d.project = $('.projects').val();
+        //         }
+        //     },
+        //     aoColumns : [
+        //         // { data: 'company_name', name: 'company_name' },
+        //         { data: 'name', name: 'name' },
+        //         { data: 'email', name: 'email' },
+        //         { data: 'phone_number', name: 'phone_number' },
+        //         { data: 'city_name', name: 'city_name' },
+        //         { data: 'project', name: 'project' },
+        //         {
+        //            data: 'is_actived', 
+        //            name: 'is_actived', 
+        //            bSortable: false,
+        //            mRender: function (data, type, full) {
+        //                 var checked = full.is_actived ? 'checked' : '';
+        //                 return `<input type="checkbox" data-developer="${full.name}" id="${full.dev_id}" class="status" switch="success" ${checked}><label for="${full.dev_id}" data-on-label="Aktif" data-off-label="Inaktif"></label>`;
+        //            },
+        //            createdCell:  function (td, cellData, rowData, row, col) {
+        //                 $(td).attr('class', 'status'); 
+        //            }
+        //         },
+        //         { data: 'action', name: 'action', bSortable: false },
+        //     ],
+        // });
+
+        // $('#btn-filter').on('click', function () {
+        //     table.fnDraw();
+        // });
 
         $(document).on('click', '.status input[type=checkbox]', function(e){
             e.preventDefault();
