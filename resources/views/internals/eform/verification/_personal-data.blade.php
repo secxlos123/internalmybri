2091d31ce6911949b88c48523f3595dde70739ab
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-10-31 15:29:21 +0700

    fixing bug

--- resources/views/internals/eform/verification/_personal-data.blade.php
+++ resources/views/internals/eform/verification/_personal-data.blade.php
@@ -73,14 +73,14 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-horizontal">
-                            <div class="form-group">
+                            <!-- <div class="form-group">
                                 <label class="col-md-3 control-label">NIK * :</label>
                                 <div class="col-md-9">
                                     <div class="input-group">
-                                        <input type="text" class="form-control" name="nik" value="{{$dataCustomer['customer']['nik']}}">
+                                        <input type="text" class="form-control" name="nik" value="{{$dataCustomer['customer']['nik']}}" readonly="">
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="form-group">
                                 <label class="col-md-3 control-label">Tempat Lahir * :</label>
                                 <div class="col-md-9">
