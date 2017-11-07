7b090cb9a41e018bad86bb48664d0fdf9c5dd6c8
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2017-11-07 10:10:42 +0700

    fix longitude latitude

--- public/assets/js/jquery.gmaps.js
+++ public/assets/js/jquery.gmaps.js
@@ -1,16 +1,39 @@
-function initialize() {
-   var latlng = new google.maps.LatLng(-6.935808,107.578095);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successMaps, errorMaps);
+} else {
+    alert('geolocation not supported');
+}
+
+function successMaps(position) {
+    google.maps.event.addDomListener(
+        window
+        , 'load'
+        , initialize(
+            position.coords.longitude
+            , position.coords.latitude
+            )
+        );
+}
+
+function errorMaps(msg) {
+    console.log(msg);
+    document.getElementById('lat').value = -6.217458;
+    document.getElementById('lng').value = 106.813880;
+}
+
+function initialize(longBase, latBase) {
+    var latlng = new google.maps.LatLng(latBase, longBase);
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: latlng,
-      zoom: 13,
-      disableDefaultUI: true
+        center: latlng,
+        zoom: 13,
+        disableDefaultUI: true
     });
     var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      draggable: true
-      // anchorPoint: new google.maps.Point(0, -29)
-   });
+        map: map,
+        position: latlng,
+        draggable: true
+// anchorPoint: new google.maps.Point(0, -29)
+});
     var input = document.getElementById('searchInput');
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     var geocoder = new google.maps.Geocoder();
@@ -26,38 +49,37 @@ function initialize() {
             return;
         }
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
-       
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);          
-    
-        bindDataToForm(place.formatted_address,place.geometry.location.lat(),place.geometry.location.lng());
-        infowindow.setContent(place.formatted_address);
-        infowindow.open(map, marker);
-       
-    });
-    // this function will work on marker move event into map 
-    google.maps.event.addListener(marker, 'dragend', function() {
-        geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
+// If the place has a geometry, then present it on a map.
+if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+} else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);
+}
+
+marker.setPosition(place.geometry.location);
+marker.setVisible(true);          
+
+bindDataToForm(place.formatted_address,place.geometry.location.lat(),place.geometry.location.lng());
+infowindow.setContent(place.formatted_address);
+infowindow.open(map, marker);
+
+});
+// this function will work on marker move event into map 
+google.maps.event.addListener(marker, 'dragend', function() {
+    geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-          if (results[0]) {        
-              bindDataToForm(results[0].formatted_address,marker.getPosition().lat(),marker.getPosition().lng());
-              infowindow.setContent(results[0].formatted_address);
-              infowindow.open(map, marker);
-          }
+            if (results[0]) {        
+                bindDataToForm(results[0].formatted_address,marker.getPosition().lat(),marker.getPosition().lng());
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
+            }
         }
-        });
     });
-  }
-  function bindDataToForm(address,lat,lng){
-   document.getElementById('location').value = address;
-   document.getElementById('lat').value = lat;
-   document.getElementById('lng').value = lng;
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+});
+}
+function bindDataToForm(address,lat,lng){
+    document.getElementById('location').value = address;
+    document.getElementById('lat').value = lat;
+    document.getElementById('lng').value = lng;
+}
