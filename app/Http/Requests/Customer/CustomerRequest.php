4ae2d81794df43d05ef69e98653e9847f49e282f
Erwan Akse <erwanakse@gmail.com>
2017-11-09 18:03:40 +0700

    fix validation add leads

--- app/Http/Requests/Customer/CustomerRequest.php
+++ app/Http/Requests/Customer/CustomerRequest.php
@@ -37,11 +37,11 @@ class CustomerRequest extends FormRequest
             'mother_name'   => 'required',
             'mobile_phone'  => 'required|string|regex:/^08[0-9]+$/|min:9|max:16',
             'identity'  => 'mimes:jpeg,jpg,png,gif|required|max:10000',
-            'couple_nik' => 'required_if:status,1',
-            'couple_name' => 'required_if:status,1',
-            'couple_birth_date' => 'required_if:status,1',
-            'couple_birth_place_id' => 'required_if:status,1',
-            'couple_identity'  => 'required_if:status,1|mimes:jpeg,jpg,png,gif|max:10000'
+            'couple_nik' => 'required_if:status,2',
+            'couple_name' => 'required_if:status,2',
+            'couple_birth_date' => 'required_if:status,2',
+            'couple_birth_place_id' => 'required_if:status,2',
+            'couple_identity'  => 'required_if:status,2|mimes:jpeg,jpg,png,gif|max:10000'
         ];
     }
 
