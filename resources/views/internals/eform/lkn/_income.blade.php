14772e0efcb00c38527f618fd925608054520164
Lisda Yuliani <lisda.yuliani@wgs.co.id>
2017-11-15 14:05:13 +0700

    fixing label lkn

--- resources/views/internals/eform/lkn/_income.blade.php
+++ resources/views/internals/eform/lkn/_income.blade.php
@@ -25,7 +25,7 @@
                                             </div>
                                         </div>
                                         <div class="form-group income_salary {!! $errors->has('income_salary') ? 'has-error' : '' !!}" id="fixed-salary">
-                                            <label class="col-md-4 control-label">Gaji / THP * :</label>
+                                            <label class="col-md-4 control-label">Gaji / THP per-Bulan * :</label>
                                             <div class="col-md-8">
                                                 <div class="input-group">
                                                     <span class="input-group-addon">Rp</span>
