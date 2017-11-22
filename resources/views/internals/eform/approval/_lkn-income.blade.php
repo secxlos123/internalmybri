00e59f47ca8b2b41c466fa1713fe0320da6bb3ce
Erwan Akse <erwanakse@gmail.com>
2017-11-22 12:53:56 +0700

    fix view approval

--- resources/views/internals/eform/approval/_lkn-income.blade.php
+++ resources/views/internals/eform/approval/_lkn-income.blade.php
@@ -7,17 +7,17 @@
             <div class="form-group">
                 <label class="col-md-5 control-label">Gaji/Penghasilan :</label>
                 <div class="col-md-7">
-                    @if (!$detail['visit_report']['income_allowance'] >= 0)
+                    @if (!$detail['visit_report']['income_salary'] >= 0)
                         <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income'], 2, ",", ".") }}</p>
                     @else
-                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_allowance'], 2, ",", ".") }}</p>
+                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_salary'], 2, ",", ".") }}</p>
                     @endif
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-md-5 control-label">Penghasilan Lain :</label>
                 <div class="col-md-7">
-                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_salary'], 2, ",", ".") }}</p>
+                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_allowance'], 2, ",", ".") }}</p>
                 </div>
             </div>
         </form>
