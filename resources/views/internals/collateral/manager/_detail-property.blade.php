db5aea5d26d15baeb44526d405de3b9b606c2a85
Lisda Yuliani <lisda.yuliani@wgs.co.id>
2017-12-06 09:37:31 +0700

    fixing image link

--- resources/views/internals/collateral/manager/_detail-property.blade.php
+++ resources/views/internals/collateral/manager/_detail-property.blade.php
@@ -28,7 +28,7 @@
                     <div class="form-group">
                         <label class="col-md-5 control-label">Foto :</label>
                         <div class="col-md-7">
-                            <img id="preview" @if(!empty($collateral['property']['photos'])) src="{{asset('assets/images/no-image.jpg')}}" @else src="{{asset('assets/images/no-image.jpg')}}" @endif width="300">
+                            <img id="preview" @if(!empty($collateral['property']['photos'])) src="{{$collateral['property']['photos']['image']}}" @else src="{{asset('assets/images/no-image.jpg')}}" @endif width="300">
                         </div>
                     </div>
                     <div class="form-group">
