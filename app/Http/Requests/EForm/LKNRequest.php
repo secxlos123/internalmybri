a27f48e9f30cfbe2438d749bcb1abc676010cf59
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2017-12-07 16:58:23 +0700

    LKN Request

--- app/Http/Requests/EForm/LKNRequest.php
+++ app/Http/Requests/EForm/LKNRequest.php
@@ -74,6 +74,13 @@ class LKNRequest extends FormRequest
            'building_permit'            => 'required_if:use_reason,2,18|mimes:jpg,png,gif,svg,rar,pdf,zip',
            'proprietary'                => 'required_if:use_reason,2,18|mimes:jpg,png,gif,svg,rar,pdf,zip',
 
+           'title'                      => 'required|number',
+           'employment_status'          => 'required|number',
+           'age_of_mpp'                 => 'required|number',
+           'loan_history_accounts'      => 'required|number',
+           'religion'                   => 'required|number',
+           'office_phone'               => 'required|string|regex:/^[0-9]+$/|min:9|max:12',
+
            'pros'                       => 'required',
            'cons'                       => 'required',
            'recommendation'             => 'required',
