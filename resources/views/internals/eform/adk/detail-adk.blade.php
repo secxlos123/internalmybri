618afa2b0686d648d7ac50e09e192a851d1135d0
Aswin <csbcan57@gmail.com>
2017-12-12 19:54:52 +0700

    fix layouts

--- resources/views/internals/eform/adk/detail-adk.blade.php
+++ resources/views/internals/eform/adk/detail-adk.blade.php
@@ -27,132 +27,178 @@
 
             <div class="row">
                 <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Exsummary</h3>
-                        </div>
-                        <!-- data exsummary -->
-                        <div class="panel-body">
-                            @include('internals.eform.adk._cus-exsummary')
-                        </div>
-                    </div>
+                    <ul class="nav nav-pills m-b-30">
+                        <li class="active">
+                            <a href="#exsummary" data-toggle="tab" aria-expanded="true">Data Exsummary</a>
+                        </li>
+                        <li>
+                            <a href="#prescoring" data-toggle="tab" aria-expanded="true">Data Prescoring</a>
+                        </li>
+                        <li>
+                            <a href="#debitur" data-toggle="tab" aria-expanded="true">Data Debitur</a>
+                        </li>
+                        <li>
+                            <a href="#kredit" data-toggle="tab" aria-expanded="true">Data Kredit</a>
+                        </li>
+                        <li>
+                            <a href="#pribadi" data-toggle="tab" aria-expanded="true">Data Pribadi</a>
+                        </li>
+                        <li>
+                            <a href="#pekerjaan" data-toggle="tab" aria-expanded="true">Data Pekerjaan</a>
+                        </li>
+                        <li>
+                            <a href="#finansial" data-toggle="tab" aria-expanded="true">Data Finansial</a>
+                        </li>
+                        <li>
+                            <a href="#keluarga" data-toggle="tab" aria-expanded="true">Data Keluarga</a>
+                        </li>
+                        <li>
+                            <a href="#dokumen" data-toggle="tab" aria-expanded="true">Data Dokumen</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Prescoring</h3>
-                        </div>
-                        <!-- data prescoring -->
-                        <div class="panel-body">
-                            @include('internals.eform.adk._cus-prescoring')
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Debitur</h3>
-                        </div>
-                        <!-- data debitur -->
-                        <div class="panel-body">
-                            @include('internals.eform.adk._cus-debitur')
+            <div class="tab-content br-n pn">
+                <div id="exsummary" class="tab-pane active">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Exsummary</h3>
+                                </div>
+                                <!-- data exsummary -->
+                                <div class="panel-body">
+                                    @include('internals.eform.adk._cus-exsummary')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Kredit</h3>
-                        </div>
-                        <!-- data kredit -->
-                        <div class="panel-body">
-                            @include('internals.eform.adk._cus-data_kredit')
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Pribadi</h3>
-                        </div>
-                        <!-- data pribadi -->
-                        <div class="panel-body">
-                            @include('internals.eform.adk._cus-personal')
-                            <hr>
-
-                            @if($detail['customer']['personal']['status_id'] == 2)
-                            <!--pasangan-->
-                            @include('internals.eform.adk._cus-couple')
-                            <hr>
-                            @endif
+                <div id="prescoring" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Prescoring</h3>
+                                </div>
+                                <!-- data prescoring -->
+                                <div class="panel-body">
+                                    @include('internals.eform.adk._cus-prescoring')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Pekerjaan</h3>
-                        </div>
-                        <div class="panel-body">
-                            <!--pekerjaan-->
-                            @include('internals.eform.adk._cus-work')
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Finansial</h3>
-                        </div>
-                        <div class="panel-body">
-                            <!-- finansial -->
-                            @include('internals.eform.adk._cus-financial')
+                <div id="debitur" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Debitur</h3>
+                                </div>
+                                <!-- data debitur -->
+                                <div class="panel-body">
+                                    @include('internals.eform.adk._cus-debitur')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Data Keluarga</h3>
-                        </div>
-                        <div class="panel-body">
-                            <!-- family -->
-                            @include('internals.eform.adk._cus-family')
+                <div id="kredit" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Kredit</h3>
+                                </div>
+                                <!-- data kredit -->
+                                <div class="panel-body">
+                                    @include('internals.eform.adk._cus-data_kredit')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div id="pribadi" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Pribadi</h3>
+                                </div>
+                                <!-- data pribadi -->
+                                <div class="panel-body">
+                                    @include('internals.eform.adk._cus-personal')
+                                    <hr>
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Briguna Dokumen</h3>
+                                    @if($detail['customer']['personal']['status_id'] == 2)
+                                    <!--pasangan-->
+                                    @include('internals.eform.adk._cus-couple')
+                                    <hr>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                        <div class="panel-body">
-                            <!-- identity -->
-                            @include('internals.eform.adk._cus-identity')
+                    </div>
+                </div>
+                <div id="pekerjaan" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Pekerjaan</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <!--pekerjaan-->
+                                    @include('internals.eform.adk._cus-work')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="finansial" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Finansial</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <!-- finansial -->
+                                    @include('internals.eform.adk._cus-financial')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="keluarga" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Data Keluarga</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <!-- family -->
+                                    @include('internals.eform.adk._cus-family')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="dokumen" class="tab-pane">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Briguna Dokumen</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <!-- identity -->
+                                    @include('internals.eform.adk._cus-identity')
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -162,7 +208,15 @@
             <form class="form-horizontal" role="form" action="{{route('postApproval', $id)}}" method="POST" id="form1">
                 {{ csrf_field() }}
                 <input type="hidden" name="is_approved" id="is_approved">
-                @include('internals.eform.approval._recommendation')
+                <div class="text-center">
+                    @if ($type != 'preview')
+                        <button type="submit" href="#" class="btn btn-orange waves-light waves-effect w-md m-b-20" id="btn-approve">Terima</button>
+                        <button type="submit" href="#" class="btn btn-danger waves-light waves-effect w-md m-b-20" id="btn-reject">Tolak</button>
+                    @else
+                        <button type="button" onclick="printPage()" class="btn waves-light waves-effect w-md m-b-20"><i class="fa fa-print"></i> Print</button>
+                    @endif
+                        <a href="{{URL::previous()}}" class="btn btn-default waves-light waves-effect w-md m-b-20">Kembali</a>
+                </div>
             </form>
         </div>
     </div>
