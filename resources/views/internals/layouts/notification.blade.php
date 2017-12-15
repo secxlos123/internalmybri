3488711952216a552f557911cec22494063913c9
iqbal qurahman <iqbal.qurahman@kiranatama.com>
2017-12-15 21:17:35 +0700

    layeout notification internal

--- resources/views/internals/layouts/notification.blade.php
+++ resources/views/internals/layouts/notification.blade.php
@@ -2,15 +2,16 @@
     <a href="#" class="right-menu-item dropdown-toggle" data-toggle="dropdown">
         <i class="mdi mdi-bell"></i>
         <span class="badge up bg-success"> 
+            {{ count( notificationsUnread() ) }}
         </span>
     </a>
     <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right dropdown-lg user-list notify-list" >
         <li>
-            <h5>Notifikasi</h5>
+            <h5><strong>Notifikasi</strong></h5>
         </li>
         
-        <li class="all-msgs text-center" style="display: none;">
-            <p class="m-0" style="display: none;"><a href="#">Lihat Semua Notifikasi</a></p>
+        <li class="all-msgs text-center" style="display: block;">
+            <p class="m-0" style="display: block;"><a> &nbsp;</a></p>
         </li>
     </ul>
 </li>
