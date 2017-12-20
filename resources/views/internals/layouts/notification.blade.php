ea89a99f43064752f7e6b3357a804cb4756a1b3b
iqbal qurahman <iqbal_felixs@yahoo.com>
2017-12-20 18:32:07 +0700

    display block notif before

--- resources/views/internals/layouts/notification.blade.php
+++ resources/views/internals/layouts/notification.blade.php
@@ -1,4 +1,4 @@
-<li style="display: none;">
+<li style="display: block;">
     <a href="#" class="right-menu-item dropdown-toggle" data-toggle="dropdown">
         <i class="mdi mdi-bell"></i>
         <span class="badge up bg-success"> 
@@ -9,6 +9,26 @@
         <li>
             <h5><strong>Notifikasi</strong></h5>
         </li>
+        @if(count(notificationsUnread()) > 0 )
+            @foreach(notificationsUnread() as $value)
+                    <li>
+                        <a href="{{ url('/eform?ref_number='.$value['data']['ref_number'].'&ids='.$value['data']['eform_id'].'') }}" class="user-list-item list-notif">
+                            <div class="icon bg-danger">
+                                <i class="mdi mdi-comment"></i>
+                            </div>
+                            <div class="user-desc">
+                                <div class="title"><strong>{{ $value['subject'] }}</strong></div>
+                                <span class="name">{{ $value['data']['user_name'] }}</span>
+                                <div class="time">{{ $value['created_at'] }}</div>                            
+                            </div>
+                        </a>
+                    </li>
+            @endforeach
+        @else
+            <li class="all-msgs text-center">
+                <p class="m-0"><a href="#">Tidak Ada Data Notifikasi</a></p>
+            </li>
+        @endif
         
         <li class="all-msgs text-center" style="display: block;">
             <p class="m-0" style="display: block;"><a> &nbsp;</a></p>
