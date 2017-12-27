2d873b26d09a204870b6181f0c1ee83e659a7a62
lisda.yuliani <lisda.yuliani@wgs.co.id>
2017-12-27 11:48:59 +0700

    fixing exception handler

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@section('title','My BRI - 404 - Halaman Tidak Ditemukan')
+@include('internals.layouts.head')
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <div class="wrapper-page csm-has-404">
+                        <img src="{{asset('assets/images/animat-search-color.gif')}}" alt="" height="200">
+                        <h2 class="text-uppercase text-danger"><b>Halaman Tidak Ditemukan</b></h2>
+                        <p class="text-muted">Maaf, halaman yang Anda tuju tidak ditemukan.</p>
+
+                        <a class="btn btn-primary waves-light waves-effect w-md m-b-15 top20" href="{{route('dashboard')}}"> Kembali ke Dashboard</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@include('internals.layouts.foot')
