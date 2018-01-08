98763d67357a8306c713ca4e6649dcbcb0f85e93
Erwan Akse <erwanakse@gmail.com>
2018-01-08 11:47:18 +0700

    add zip and pdf on lkn file

--- resources/views/internals/eform/lkn/_common.blade.php
+++ resources/views/internals/eform/lkn/_common.blade.php
@@ -5,20 +5,20 @@
                 <div class="form-group">
                     <label class="col-md-6 control-label">Foto NPWP * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="npwp" accept="image/*">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="npwp" accept="image/*,application/pdf,application/rar,application/zip">
                         @if ($errors->has('npwp')) <p class="help-block">{{ $errors->first('npwp') }}</p> @endif
                     </div>
                 </div>
                 <div class="form-group" id="salary_slip">
                     <label class="col-md-6 control-label">Slip Gaji * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="salary_slip" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="salary_slip" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group" id="legal_bussiness_document">
                     <label class="col-md-6 control-label">Dokumen Legal Usaha * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="legal_bussiness_document" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="legal_bussiness_document" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <!-- <div class="form-group">
@@ -30,20 +30,20 @@
                 <div class="form-group" id="work_letter">
                     <label class="col-md-6 control-label">Surat Keterangan Kerja * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="work_letter" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="work_letter" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-6 control-label">Kartu Keluarga * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="family_card" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="family_card" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 @if($eformData['customer']['personal']['status_id'] >= 2)
                 <div class="form-group" id="marrital_certificate">
                     <label class="col-md-6 control-label">Akta Nikah/Akta Cerai :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="marrital_certificate" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="marrital_certificate" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 @endif
@@ -57,7 +57,7 @@
                 <div class="form-group" id="separate_certificate">
                     <label class="col-md-6 control-label">Akta Pisah Harta :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="divorce_certificate" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="divorce_certificate" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 @endif
@@ -70,37 +70,37 @@
                 <div class="form-group">
                     <label class="col-md-6 control-label">Surat Penawaran * :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="offering_letter" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="offering_letter" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group" id="shm" @if($eformData['kpr']['developer_id'] != 1) hidden="" @endif>
                     <label class="col-md-6 control-label">Sertifikat Hak Milik :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="proprietary" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="proprietary" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group" id="imb" @if($eformData['kpr']['developer_id'] != 1) hidden="" @endif>
                     <label class="col-md-6 control-label">Izin Mendirikan Bangunan :</label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="building_permit" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="building_permit" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-6 control-label">Bukti DP </label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="down_payment" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="down_payment" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-6 control-label">Pajak Bumi dan Bangunan Terakhir </label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="building_tax" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="building_tax" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
                 <div class="form-group">
                     <label class="col-md-6 control-label">Dokumen Lainnya </label>
                     <div class="col-md-6">
-                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="other_document" accept="image/*,application/pdf,application/rar">
+                        <input type="file" class="filestyle" data-buttontext="Unggah" data-buttonname="btn-default" data-iconname="glyphicon glyphicon-folder-open" data-placeholder="Tidak ada file" name="other_document" accept="image/*,application/pdf,application/rar,application/zip">
                     </div>
                 </div>
             </div>
