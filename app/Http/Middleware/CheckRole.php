d374c4ac25bba647dc079d8ee2012d3e45e4ccd5
Erwan Akse <erwanakse@gmail.com>
2018-01-10 14:02:55 +0700

    add base middleware for guard url

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next , $role)
+    {
+        $data = $this->getUser();
+        if ($data['role'] != $role) {
+            return redirect()->route('dashboard');
+        }
+        return $next($request);
+    }
+
+    public function getUser(){
+
+     /* GET UserLogin Data */
+        $users = session()->get('user');
+            foreach ($users as $user) {
+                $data = $user;
+            }
+        return $data;
+    }
+}
