19c6683d23301e83b781d2235b7ca655646fd52a
lisda.yuliani <lisda.yuliani@wgs.co.id>
2018-01-11 16:13:41 +0700

    fixing detail customer identity

--- resources/views/internals/customers/detail.blade.php
+++ resources/views/internals/customers/detail.blade.php
@@ -249,7 +249,7 @@
                                                             <div class="form-group">
                                                                 <label class="col-md-6 control-label">Lama Kerja :</label>
                                                                 <div class="col-md-6">
-                                                                    <p class="form-control-static">@if(!empty($dataCustomer['work']['work_duration'])){{$dataCustomer['work']['work_duration']}} Tahun {{$dataCustomer['work']['work_duration_month']}} Bulan @else - @endif</p>
+                                                                    <p class="form-control-static">@if(isset($dataCustomer['work']['work_duration'])){{$dataCustomer['work']['work_duration']}} Tahun {{$dataCustomer['work']['work_duration_month']}} Bulan @else - @endif</p>
                                                                 </div>
                                                             </div>
                                                             <div class="form-group">
@@ -417,15 +417,34 @@
                                                 <div class="row">
                                                     <div class="col-md-6" align="center">
                                                         <div class="card-box">
-                                                            <img src="@if(!empty($dataCustomer['other']['identity'])){{$dataCustomer['other']['identity']}}@endif" class="img-responsive">
-                                                            <p>Foto KTP</p>
+                                                            @if((pathinfo(strtolower($dataCustomer['other']['identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($dataCustomer['other']['identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($dataCustomer['other']['identity'])), PATHINFO_EXTENSION) == 'jpeg'))
+                                                                @if(strpos($dataCustomer['other']['identity'], 'noimage.jpg'))
+                                                                <p>Foto KTP Kosong</p>
+                                                                @else
+                                                                <img src="@if(!empty($dataCustomer['other']['identity'])){{$dataCustomer['other']['identity']}}@endif" class="img-responsive">
+                                                                <p>Foto KTP</p>
+                                                                @endif
+                                                            @else
+                                                                <a href="@if(!empty($dataCustomer['other']['identity'])){{$dataCustomer['other']['identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
+                                                                <p>Klik Untuk Lihat Foto KTP</p>
+                                                            @endif
                                                         </div>
                                                     </div>
                                                     @if(!empty($dataCustomer['personal']['status_id']) &&($dataCustomer['personal']['status_id'] == 2))
                                                     <div class="col-md-6" align="center">
                                                         <div class="card-box">
-                                                            <img src="@if(!empty($dataCustomer['personal']['couple_identity'])){{$dataCustomer['personal']['couple_identity']}}@endif" class="img-responsive">
-                                                            <p>Foto KTP Pasangan</p>
+                                                            @if((pathinfo(strtolower($dataCustomer['personal']['couple_identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($dataCustomer['personal']['couple_identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($dataCustomer['personal']['couple_identity'])), PATHINFO_EXTENSION) == 'jpeg'))
+                                                                @if(strpos($dataCustomer['personal']['couple_identity'], 'noimage.jpg'))
+                                                                <p>Foto KTP Pasangan Kosong</p>
+                                                                @else
+                                                                <img src="@if(!empty($dataCustomer['personal']['couple_identity'])){{$dataCustomer['personal']['couple_identity']}}@endif" class="img-responsive">
+                                                                <p>Foto KTP Pasangan</p>
+                                                                @endif
+                                                            @else
+                                                                <a href="@if(!empty($dataCustomer['personal']['couple_identity'])){{$dataCustomer['personal']['couple_identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
+                                                                <p>Klik Untuk Lihat Foto KTP Pasangan</p>
+                                                            @endif
+                                                            
                                                         </div>
                                                     </div>
                                                     @endif
