706893fc25b975dbfa165dbe15c6c210b1a9657d
lisda.yuliani <lisda.yuliani@wgs.co.id>
2018-01-15 11:16:52 +0700

    fixing label of MyBRI in layout and authenticate

--- resources/views/internals/home/index.blade.php
+++ resources/views/internals/home/index.blade.php
@@ -1,4 +1,4 @@
-@section('title','My BRI - Dashboard')
+@section('title','MyBRI - Dashboard')
 @include('internals.layouts.head')
 @include('internals.layouts.header')
 @include('internals.layouts.navigation')
@@ -8,10 +8,10 @@
             <div class="row">
              <div class="col-xs-12">
                 <div class="page-title-box">
-                    <h4 class="page-title">Home </h4>
+                    <h4 class="page-title">Home MyBRI </h4>
                     <ol class="breadcrumb p-0 m-0">
                         <li>
-                            <a href="#">Home</a>
+                            <a href="#">Home MyBRI</a>
                         </li>
                         <li>
                             <a href="#">Dashboard</a>
