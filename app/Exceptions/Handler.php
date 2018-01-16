12949a3ae2f2b190cf90912da4835fb582044b24
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-01-16 14:22:56 +0700

    change subject

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,7 +49,7 @@ class Handler extends ExceptionHandler
         if ( ENV("APP_ENV") == "production" ) {
             Mail::send('mails.ErrorException', array('exception' => $exception), function($message)
             {
-                $message->subject("INT-myBRI Error Exception");
+                $message->subject("INT myBRI Error Exception");
                 $message->from("error@mybri.bri.co.id", 'Error Exception');
                 $message->to("rachmat.ramadhan@wgs.co.id");
             });
