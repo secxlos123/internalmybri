2698ff454d8dedd20915fd212a7e723a26c17f49
Erwan Akse <erwanakse@gmail.com>
2018-01-22 19:09:36 +0700

    fix validation on createOts

--- app/Http/Requests/Collateral/LKNRequest.php
+++ app/Http/Requests/Collateral/LKNRequest.php
@@ -33,6 +33,7 @@ class LKNRequest extends FormRequest
           ->merge($this->environmentRules())
           ->merge($this->sevenRules())
           ->merge($this->eightRules())
+          ->merge($this->nineRules())
           ->merge($this->tenRules())
           ->toArray();
     }
@@ -45,17 +46,17 @@ class LKNRequest extends FormRequest
     {
       return [
         'area.collateral_type' => 'required',
-        'area.city_id' => 'required',
+        'area.city_id' => 'required|regex:/^[\d.]+$/|exists:cities,id',
         'area.location' => 'required',
         'area.latitude' => 'required',
         'area.longtitude' => 'required',
         'area.district' => 'required',
         'area.sub_district' => 'required',
-        'area.rt' => 'required',
-        'area.rw' => 'required',
-        'area.zip_code' => 'required',
-        'area.distance' => 'required',
-        'area.unit_type' => 'required',
+        'area.rt' => 'required|numeric',
+        'area.rw' => 'required|numeric',
+        'area.zip_code' => 'required|numeric',
+        'area.distance' => 'required|numeric',
+        'area.unit_type' => 'required|numeric',
         'area.distance_from' => 'required',
         'area.position_from_road' => 'required',
         'area.ground_type' => 'required',
@@ -86,7 +87,7 @@ class LKNRequest extends FormRequest
         'letter.number' => 'required',
         'letter.date' => 'required|date',
         'letter.on_behalf_of' => 'required',
-        'letter.duration_land_authorization' => 'required',
+        'letter.duration_land_authorization' => 'date',
         'letter.bpn_name' => 'required'
       ];
     }
@@ -125,20 +126,20 @@ class LKNRequest extends FormRequest
     {
       return [
         'valuation.scoring_land_date' => 'required|date',
-        'valuation.npw_land' => 'required',
-        'valuation.nl_land' => 'required',
-        'valuation.pnpw_land' => 'required',
-        'valuation.pnl_land' => 'required',
+        'valuation.npw_land' => 'required|numeric',
+        'valuation.nl_land' => 'required|numeric',
+        'valuation.pnpw_land' => 'required|numeric',
+        'valuation.pnl_land' => 'required|numeric',
         'valuation.scoring_building_date' => 'required|date',
-        'valuation.npw_building' => 'required',
-        'valuation.nl_building' => 'required',
-        'valuation.pnpw_building' => 'required',
-        'valuation.pnl_building' => 'required',
+        'valuation.npw_building' => 'required|numeric',
+        'valuation.nl_building' => 'required|numeric',
+        'valuation.pnpw_building' => 'required|numeric',
+        'valuation.pnl_building' => 'required|numeric',
         'valuation.scoring_all_date' => 'required|date',
-        'valuation.npw_all' => 'required',
-        'valuation.nl_all' => 'required',
-        'valuation.pnpw_all' => 'required',
-        'valuation.pnl_all' => 'required'
+        'valuation.npw_all' => 'required|numeric',
+        'valuation.nl_all' => 'required|numeric',
+        'valuation.pnpw_all' => 'required|numeric',
+        'valuation.pnl_all' => 'required|numeric'
       ];
     }
 
@@ -154,7 +155,8 @@ class LKNRequest extends FormRequest
         'other.optimal_building_use' => 'required',
         'other.building_exchange' => 'required',
         'other.things_bank_must_know' => 'required',
-        'other.image_condition_area' => 'required|file'
+        'other.image_area.*.image_data' => 'required|mimes:jpeg,png,jpg,zip,pdf',
+        'other.image_condition_area' => 'mimes:jpeg,png,jpg,zip,pdf'
       ];
     }
 
@@ -166,7 +168,10 @@ class LKNRequest extends FormRequest
     {
       return [
         'environment.designated_land' => 'required',
-        'environment.designated' => 'required',
+        'environment.designated_pln' => '',
+        'environment.designated_phone' => '',
+        'environment.designated_pam' => '',
+        'environment.designated_telex' => '',
         'environment.other_designated' => 'required',
         'environment.nearest_location' => 'required',
         'environment.other_guide' => 'required',
@@ -175,57 +180,95 @@ class LKNRequest extends FormRequest
       ];
     }
 
-    /**
-     * seven rules
+     /**
+     * environment rules
      * @return array
      */
     private function sevenRules()
     {
       return [
         'seven.collateral_status' => 'required',
-        'seven.on_behalf_of' => 'required',
-        'seven.ownership_number' => 'required',
-        'seven.address_collateral' => 'required',
+        'seven.on_behalf_of' => '',
+        'seven.ownership_number' => '',
+        'seven.location' => '',
+        'seven.address_collateral' => '',
+        'seven.description' => 'required',
         'seven.ownership_status' => 'required',
-        'seven.date_evidence' => 'required',
+        'seven.date_evidence' => 'required|date',
         'seven.village' => 'required',
         'seven.districts' => 'required'
       ];
     }
 
-    /**
-     * eight rules
+     /**
+     * environment rules
      * @return array
      */
     private function eightRules()
     {
       return [
-        'eight.liquidation_realization' => 'required',
-        'eight.fair_market' => 'required',
-        'eight.liquidation' => 'required',
-        'eight.fair_market_projection' => 'required',
-        'eight.liquidation_projection' => 'required',
-        'eight.njop' => 'required',
-        'eight.appraisal_by' => 'required',
+        'eight.liquidation_realization' => 'required|numeric',
+        'eight.fair_market' => 'required|numeric',
+        'eight.liquidation' => 'required|numeric',
+        'eight.fair_market_projection' => 'required|numeric',
+        'eight.liquidation_projection' => 'required|numeric',
+        'eight.njop' => 'required|numeric',
+        'eight.appraisal_by' => 'required|in:Bank,Lembaga Penilai',
+        'eight.independent_appraiser' => 'required_if:eight.appraisal_by,Lembaga Penilai',
+        'eight.independent_appraiser_name' => 'required_if:eight.appraisal_by,Lembaga Penilai',
+        'eight.date_assessment' => 'required|date',
         'eight.type_binding' => 'required',
         'eight.binding_number' => 'required',
-        'eight.binding_value' => 'required'
+        'eight.binding_value' => 'required|numeric',
+
+      ];
+    }
+
+     /**
+     * environment rules
+     * @return array
+     */
+    private function nineRules()
+    {
+      return [
+        'nine.certificate_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.receipt_date' => 'required_if:nine.certificate_status,Sudah Diberikan|date',
+        'nine.information' => 'required_if:nine.certificate_status,Sudah Diberikan',
+        'nine.notary_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.takeover_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.credit_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.skmht_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.imb_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.shgb_status' => 'required|in:Sudah Diberikan,Belum Diberikan',
+        'nine.receipt_date_notary'=>'required_if:nine.notary_status,Sudah Diberikan|date',
+        'nine.information_notary'=>'required_if:nine.notary_status,Sudah Diberikan',
+        'nine.receipt_date_takeover'=>'required_if:nine.takeover_status,Sudah Diberikan|date',
+        'nine.information_takeover'=>'required_if:nine.takeover_status,Sudah Diberikan',
+        'nine.receipt_date_credit'=>'required_if:nine.credit_status,Sudah Diberikan|date',
+        'nine.information_credit'=>'required_if:nine.credit_status,Sudah Diberikan',
+        'nine.receipt_date_skmht'=>'required_if:nine.skmht_status,Sudah Diberikan|date',
+        'nine.information_skmht'=>'required_if:nine.skmht_status,Sudah Diberikan',
+        'nine.receipt_date_imb'=>'required_if:nine.imb_status,Sudah Diberikan|date',
+        'nine.information_imb'=>'required_if:nine.imb_status,Sudah Diberikan',
+        'nine.receipt_date_shgb'=>'required_if:nine.shgb_status,Sudah Diberikan|date',
+        'nine.information_shgb' =>'required_if:nine.shgb_status,Sudah Diberikan'
       ];
     }
 
     /**
-     * ten rules
+     * environment rules
      * @return array
      */
     private function tenRules()
     {
       return [
         'ten.paripasu' => 'required',
-        'ten.paripasu_bank' => 'required',
-        'ten.insurance' => 'required',
-        'ten.insurance_company' => 'required',
-        'ten.insurance_value' => 'required',
-        'ten.eligibility' => 'required'
+        'ten.paripasu_bank' => 'numeric',
+        'ten.insurance' => 'required|in:Ya,Tidak',
+        'ten.insurance_company' => 'required_if:ten.insurance,Ya',
+        'ten.insurance_company_name' => 'required_if:ten.insurance,Ya',
+        'ten.insurance_value' => 'required_if:ten.insurance,Ya|numeric',
+        'ten.eligibility' => 'required',
       ];
     }
 }
