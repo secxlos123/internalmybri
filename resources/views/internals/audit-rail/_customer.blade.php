ec0d32f529ed2328d5ee4deb9ec68bc45a20f254
lisda.yuliani <lisda.yuliani@wgs.co.id>
2018-01-25 23:14:54 +0700

    implement UI audit rails

---
+++ resources/views/internals/audit-rail/_customer.blade.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-8">
+        <form class="form-horizontal" role="form" onsubmit="return false;">
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Nama / NIK :</label>
+                <div class="col-sm-8">
+                    <input type="text" class="form-control" name="ref_number" id="ref_number">
+                </div>
+                <div class="col-sm-2">
+                    <a href="javascript:void(0);" class="btn btn-orange waves-light waves-effect w-md" id="btn-filter">Filter</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="table-responsive">
+    <table id="datatable" class="table table-bordered responsive">
+        <thead class="bg-primary">
+            <tr>
+                <th>Old Value</th>
+                <th>New Value</th>
+                <th>Nominal</th>
+                <th>Tanggal Pengajuan</th>
+                <th>No. HP</th>
+                <th>Status Prescreening</th>
+                <th>id</th>
+                <th>Status Pengajuan</th>
+                <th>Umur Pengajuan</th>
+                <th>Janji Temu</th>
+                <th>Status Data Nasabah</th>
+                <th style="width: 100px">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr role="row" class="odd">
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ resources/views/internals/audit-rail/_profilEdit.blade.php
@@ -0,0 +1,56 @@
+<div id="filter" class="m-b-15">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card-box">
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">Tanggal Aksi :</label>
+                        <div class="col-sm-8">
+                            <div class="input-group">
+                                <input type="text" class="form-control datepicker-autoclose" name="action_date">
+                                <span class="input-group-addon b-0"><i class="mdi mdi-calendar"></i></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">Nama User :</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="customer_name">
+                        </div>
+                    </div>
+                </form>
+            <div class="text-right">
+                <a href="javascript:void(0);" class="btn btn-orange waves-light waves-effect w-md" id="btn-filter">Filter</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="table-responsive">
+    <table id="datatable" class="table table-bordered responsive">
+        <thead class="bg-primary">
+            <tr>
+                <th>Tanggal</th>
+                <th>Nama Modul</th>
+                <th>Nama User</th>
+                <th>Data Lama</th>
+                <th>Data Baru</th>
+                <th>IP Address</th>
+                <th>Lokasi Akses</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr role="row" class="odd">
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
