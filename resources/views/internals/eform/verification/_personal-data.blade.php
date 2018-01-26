b31f757b016445d3eb901f6e2a0dcfe40be7a43c
Erwan Akse <erwanakse@gmail.com>
2018-01-26 15:05:34 +0700

    Add Field Zip Code On Verification

--- resources/views/internals/eform/verification/_personal-data.blade.php
+++ resources/views/internals/eform/verification/_personal-data.blade.php
@@ -121,6 +121,17 @@
                                 </div>
                                 <input type="hidden" id="new_city" value="{{ $dataCustomer['customer']['city'] }}">
                             </div>
+                            <div class="form-group">
+                                <label class="col-md-3 control-label">Kode Pos * :</label>
+                                <div class="col-md-3">
+                                @if ($type != 'preview')
+                                    <input type="text" class="form-control numericOnly" name="zip_code" maxlength="5" value="{{(isset($dataCustomer['customer']['zip_code']) ? $dataCustomer['customer']['zip_code'] : old('zip_code'))}}">
+                                @else
+                                    <p>{{@$dataCustomer['customer']['zip_code']}}</p>
+                                @endif
+                                    @if ($errors->has('zip_code')) <p class="help-block">{{ $errors->first('zip_code') }}</p> @endif
+                                </div>
+                            </div>
 
                             @if ($type != 'preview')
                             <div class="form-group">
