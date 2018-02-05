94b3f961acd919713d96748c744c69ef83ef73f3
Aswin <csbcan57@gmail.com>
2018-02-05 17:50:54 +0700

    fix bugs

--- resources/views/internals/eform/adk/_cus-exsummary.blade.php
+++ resources/views/internals/eform/adk/_cus-exsummary.blade.php
@@ -16,7 +16,13 @@
             <div class="form-group">
                 <label class="col-md-5 control-label">CRS :</label>
                 <div class="col-md-7">
-                    <p class="form-control-static">{{$detail['score']}} (DITERIMA)</p>
+                    <p class="form-control-static">{{$detail['score']}} 
+                        @if($detail['cutoff'] == 'Y')
+                            (DITERIMA)
+                        @else
+                            (DITOLAK)
+                        @endif
+                    </p>
                 </div>
             </div>
             <div class="form-group">
