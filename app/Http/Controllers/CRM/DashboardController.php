110d832c87d914e5d2af435aab39d418786b3bdf
Achmad Aliyudin <achmadaliyudin1992@gmail.com>
2018-02-15 16:36:27 +0700

    add fo dashboard

--- app/Http/Controllers/CRM/DashboardController.php
+++ app/Http/Controllers/CRM/DashboardController.php
@@ -35,6 +35,7 @@ class DashboardController extends Controller
       'Content-Type' => 'application/json'
     ])
     ->get();
+
     $getPemasar = Client::setEndpoint('crm/pemasar')
     ->setHeaders([
       'pn' => $data['pn'],
@@ -43,10 +44,34 @@ class DashboardController extends Controller
       'Content-Type' => 'application/json'
     ])
     ->post();
+
+    $marketingByBranch = Client::setEndpoint('crm/marketing/by_branch')
+    ->setHeaders([
+      'pn' => $data['pn'],
+      'branch' => $data['branch'],
+      'Authorization' => $data['token'],
+      'Content-Type' => 'application/json'
+    ])
+    ->post();
+
+    $marketing = Client::setEndpoint('crm/marketing')
+    ->setHeaders([
+      'pn' => $data['pn'],
+      'branch' => $data['branch'],
+      'Authorization' => $data['token'],
+      'Content-Type' => 'application/json'
+    ])
+    ->get();
+
     $product = $crmIndex['contents']['product_type'];
     $pemasar = $getPemasar['contents'];
+    if (($data['role']=='pincapem') || ($data['role']=='ampd') || ($data['role']=='mp') || ($data['role']=='pinca')) {
+      $marketings = $marketingByBranch['contents'];
+    } elseif ($data['role'] == 'ao' || $data['role'] == 'fo') {
+      $marketings = $marketing['contents'];
+    }
 
-    return view('internals.crm.dashboard.index', compact('data', 'product', 'pemasar'));
+    return view('internals.crm.dashboard.index', compact('data', 'product', 'pemasar', 'marketings'));
   }
 
   public function chartMarketing(Request $request)
@@ -59,7 +84,7 @@ class DashboardController extends Controller
     $pemasar = $request->input('pemasar');
     $product = $request->input('product');
 
-    if ($data['role'] == 'pinca') {
+    if (($data['role']=='pincapem') || ($data['role']=='ampd') || ($data['role']=='mp') || ($data['role']=='pinca')) {
       $pn = $pemasar;
     } elseif ($data['role'] == 'ao' || $data['role'] == 'fo') {
       $pn = $data['pn'];
@@ -67,7 +92,7 @@ class DashboardController extends Controller
 
     // return $product;
 
-    $chartData = Client::setEndpoint('crm/marketing_summary')
+    $chartData = Client::setEndpoint('crm/marketing_summary_v2')
     ->setQuery(['role' => $data['role']])
     ->setHeaders([
       'Authorization' => $data['token'],
@@ -82,6 +107,43 @@ class DashboardController extends Controller
     ->post();
 
     return $chartData['contents'];
+  }
+
+  public function chartTotal(Request $request)
+  {
+    /* GET UserLogin Data */
+    $data = $this->getUser();
+
+    // return $product;
+
+    $chartData = Client::setEndpoint('crm/marketing_summary_v2')
+    ->setQuery(['role' => $data['role']])
+    ->setHeaders([
+      'Authorization' => $data['token'],
+      'pn' => $data['pn'],
+      'branch' => $data['branch']
+    ])
+    ->setBody([
+      "product_type"=>"", //filter pruduk
+      "month"=>"",//filter bulan
+      "pn"=>"" //filter officer
+    ])
+    ->post();
+
+    $totalData = array();
+
+    $totalData[0]['Total'] = array_sum(array_column($chartData['contents'], 'Total'));
+    $totalData[0]['Prospek'] = array_sum(array_column($chartData['contents'], 'Prospek'));
+    $totalData[0]['On Progress'] = array_sum(array_column($chartData['contents'], 'On Progress'));
+    $totalData[0]['Done'] = array_sum(array_column($chartData['contents'], 'Done'));
+    $totalData[0]['Index'] = 'All';
+    // return array_column($chartData['contents'], 'Total');
+    // foreach ($chartData['contents'] as $k=>$subArray) {
+    //   unset($subArray['Nama']);
+    //   unset($subArray['Pemasar']);
+    // }
+
+    return $totalData;
   }
 
 }
--- resources/views/internals/crm/dashboard/fo/script.blade.php
+++ resources/views/internals/crm/dashboard/fo/script.blade.php
@@ -13,7 +13,7 @@
 // //        xkey: 'pemasar',
 // //        ykeys: ['a', 'c', 'd', 'b'],
 // //        stacked: false,
-// //        labels: ['Prospek', 'On Progress', 'Batal', 'Done'],
+// //        labels: ['Total', 'Prospek', 'On Progress', 'Done'],
 // //        hideHover: 'auto',
 // //        resize: true, //defaulted to true
 // //        gridLineColor: '#eeeeee',
@@ -32,7 +32,7 @@
 //       xkey: 'pemasar',
 //       ykeys: ['a', 'c', 'd', 'b'],
 //       stacked: false,
-//       labels: ['Prospek', 'On Progress', 'Batal', 'Done'],
+//       labels: ['Total', 'Prospek', 'On Progress', 'Done'],
 //       hideHover: 'auto',
 //       resize: true, //defaulted to true
 //       gridLineColor: '#eeeeee',
@@ -91,17 +91,17 @@
       dataType: "json",
       success: function (data) {
         console.log(data);
-        MorrisCharts.prototype.createStackedChart('morris-bar-stacked', data, 'Nama', ['Prospek', 'On Progress', 'Batal', 'Done'], ['Prospek', 'On Progress', 'Batal', 'Done'], ['blue','orange', 'red','green']);
+        MorrisCharts.prototype.createStackedChart('morris-bar-stacked', data, 'Nama', ['Total', 'Prospek', 'On Progress', 'Done'], ['Total', 'Prospek', 'On Progress', 'Done'], ['blue','orange', 'yellow', 'green']);
       },
     });
     $.ajax({
-      url: "{{url('chartProperty')}}",
-      type: "GET",
+      url: "{{url('chartTotal')}}",
+      type: "POST",
+      data: data,
       dataType: "json",
       success: function (data) {
-        var $stckedData = data;
-
-        MorrisCharts.prototype.createStackedChart('chart-all', $stckedData, 'month', ['value'], ['Properti Baru'], ['#00529C']);
+        console.log(data);
+        MorrisCharts.prototype.createStackedChart('chart-all', data, 'Index', ['Total', 'Prospek', 'On Progress', 'Done'], ['Total', 'Prospek', 'On Progress', 'Done'], ['blue','orange', 'yellow', 'green']);
       },
     });
   },
@@ -122,6 +122,7 @@ function($) {
     //   "product" : product
     // ];
       $("#morris-bar-stacked").empty();
+      $("#chart-all").empty();
       $.MorrisCharts.init(bulan, pemasar, product);
       console.log();
   });
@@ -136,6 +137,7 @@ function($) {
     //   "product" : product
     // ];
       $("#morris-bar-stacked").empty();
+      $("#chart-all").empty();
       $.MorrisCharts.init(bulan, pemasar, product);
       console.log(bulan, pemasar, product);
   });
@@ -150,6 +152,7 @@ function($) {
     //   "product" : product
     // ];
       $("#morris-bar-stacked").empty();
+      $("#chart-all").empty();
       $.MorrisCharts.init(bulan, pemasar, product);
       console.log(bulan, pemasar, product);
   });
@@ -157,92 +160,94 @@ function($) {
   // console.log('test');
 }(window.jQuery);
 
-
 $(document).ready(function() {
-   var table = $('#datatable').dataTable({
-    "searching": false,
-    "serverSide": true,
-    "ajax" : {
-        url : '/datatables/customers',
-        data : function(d, settings){
-            var api = new $.fn.dataTable.Api(settings);
-
-            d.page = Math.min(
-                Math.max(0, Math.round(d.start / api.page.len())),
-                api.page.info().pages
-                );
-        }
-    },
-    "aoColumns" : [
-    { data: "nik", name: 'nik' },
-    { data: "name", name: 'name' },
-    { data: "email", name: 'email' },
-    { data: "city", name: 'city' },
-    { data: "phone", name: 'phone' },
-    { data: "gender", name: 'gender' },
-                // { data: "action", name: 'action', bSortable: false },
-                ],
-            });
-   $('#datatable_paginate').css("display", "none");
-   $('#datatable_info').css("display", "none");
-   $('#datatable_length').css("display", "none");
+  $('#datatable').dataTable();
 });
-
-$('#btn-filter').on('click', function () {
-    table.fnDraw();
-});
-
-var table2 = $('#datatable-property').DataTable({
-    searching: false,
-    "language": {
-        "emptyTable": "No data available in table"
-    }
-});
-
-$(document).on('click', "#btn-filter", function(){
-    table2.destroy();
-    reloadData1($('#city').val());
-})
-
-function reloadData1(city)
-{
-    table2 = $('#datatable-property').DataTable({
-       processing : true,
-       serverSide : true,
-       lengthMenu: [
-       [ 10, 25, 50, -1 ],
-       [ '10', '25', '50', 'All' ]
-       ],
-       language : {
-        infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
-    },
-    ajax : {
-        url : '/datatables/collateral',
-        data : function(d, settings){
-            var api = new $.fn.dataTable.Api(settings);
-
-            d.page = Math.min(
-                Math.max(0, Math.round(d.start / api.page.len())),
-                api.page.info().pages
-                );
-
-                // d.start_date = from;
-                // d.end_date = to;
-                // d.status = status;
-            }
-        },
-        aoColumns : [
-        {   data: 'prop_name', name: 'prop_name', bSortable: false  },
-        {   data: 'prop_city_name', name: 'prop_city_name',  bSortable: false  },
-        {   data: 'prop_types', name: 'prop_types',  bSortable: false  },
-        {   data: 'prop_items', name: 'prop_items', bSortable: true },
-                // {   data: 'product_type', name: 'product_type' },
-                {   data: 'prop_pic_name', name: 'prop_pic_name', bSortable: false },
-                {   data: 'prop_pic_phone', name: 'prop_pic_phone', bSortable: false },
-                // {   data: 'staff_name', name: 'staff_name', bSortable: false },
-                // {   data: 'status', name: 'status', bSortable: true },
-                {   data: 'action', name: 'action', orderable: false, searchable: false}
-                ],
-            });
-}
+// $(document).ready(function() {
+//    var table = $('#datatable').dataTable({
+//     "searching": false,
+//     "serverSide": true,
+//     "ajax" : {
+//         url : '/datatables/customers',
+//         data : function(d, settings){
+//             var api = new $.fn.dataTable.Api(settings);
+//
+//             d.page = Math.min(
+//                 Math.max(0, Math.round(d.start / api.page.len())),
+//                 api.page.info().pages
+//                 );
+//         }
+//     },
+//     "aoColumns" : [
+//     { data: "nik", name: 'nik' },
+//     { data: "name", name: 'name' },
+//     { data: "email", name: 'email' },
+//     { data: "city", name: 'city' },
+//     { data: "phone", name: 'phone' },
+//     { data: "gender", name: 'gender' },
+//                 // { data: "action", name: 'action', bSortable: false },
+//                 ],
+//             });
+//    $('#datatable_paginate').css("display", "none");
+//    $('#datatable_info').css("display", "none");
+//    $('#datatable_length').css("display", "none");
+// });
+//
+// $('#btn-filter').on('click', function () {
+//     table.fnDraw();
+// });
+//
+// var table2 = $('#datatable-property').DataTable({
+//     searching: false,
+//     "language": {
+//         "emptyTable": "No data available in table"
+//     }
+// });
+//
+// $(document).on('click', "#btn-filter", function(){
+//     table2.destroy();
+//     reloadData1($('#city').val());
+// })
+//
+// function reloadData1(city)
+// {
+//     table2 = $('#datatable-property').DataTable({
+//        processing : true,
+//        serverSide : true,
+//        lengthMenu: [
+//        [ 10, 25, 50, -1 ],
+//        [ '10', '25', '50', 'All' ]
+//        ],
+//        language : {
+//         infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
+//     },
+//     ajax : {
+//         url : '/datatables/collateral',
+//         data : function(d, settings){
+//             var api = new $.fn.dataTable.Api(settings);
+//
+//             d.page = Math.min(
+//                 Math.max(0, Math.round(d.start / api.page.len())),
+//                 api.page.info().pages
+//                 );
+//
+//                 // d.start_date = from;
+//                 // d.end_date = to;
+//                 // d.status = status;
+//             }
+//         },
+//         aoColumns : [
+//         {   data: 'prop_name', name: 'prop_name', bSortable: false  },
+//         {   data: 'prop_city_name', name: 'prop_city_name',  bSortable: false  },
+//         {   data: 'prop_types', name: 'prop_types',  bSortable: false  },
+//         {   data: 'prop_items', name: 'prop_items', bSortable: true },
+//                 // {   data: 'product_type', name: 'product_type' },
+//                 {   data: 'prop_pic_name', name: 'prop_pic_name', bSortable: false },
+//                 {   data: 'prop_pic_phone', name: 'prop_pic_phone', bSortable: false },
+//                 // {   data: 'staff_name', name: 'staff_name', bSortable: false },
+//                 // {   data: 'status', name: 'status', bSortable: true },
+//                 {   data: 'action', name: 'action', orderable: false, searchable: false}
+//                 ],
+//             });
+// }
 </script>
