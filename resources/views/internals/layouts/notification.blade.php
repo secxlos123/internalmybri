e817508af569c77338a48bc676aeafa7220427a2
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-02-21 16:18:55 +0700

    fix css notif

--- resources/views/internals/layouts/notification.blade.php
+++ resources/views/internals/layouts/notification.blade.php
@@ -7,7 +7,7 @@
         <table class="notification">
             @if(count(notificationsUnread()) > 0 )
                 @foreach(notificationsUnread() as $value)
-                    <tr class="line-notif" data-href="{{ $value['url'] }}" style="background-color: {{ $value['read_at'] ? 'rgba(144, 144, 160, 0.39)' : 'white' }};">
+                    <tr class="line-notif {{ !$value['read_at'] ? 'read-notif' : '' }}" data-href="{{ $value['url'] }}">
                           <td>
                               <div class="notif-ico bg-success">
                                   <i class="fa fa-bell"></i>
