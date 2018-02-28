712b4262c32d048014580a2af396f0f8bb85e6dc
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-02-28 18:07:56 +0700

    fix UI

--- resources/views/internals/eform/prescreening/index.blade.php
+++ resources/views/internals/eform/prescreening/index.blade.php
@@ -96,51 +96,53 @@
                 </div>
             </div>
 
-            <div class="col-md-12">
-                <div class="panel panel-color panel-primary">
-                    <div class="panel-heading" data-toggle="collapse" href="#collapseImage" style="cursor: pointer;">
-                        <h3 class="panel-title">Dokumen Sumber <small>(klik disini untuk melihat detail dokumen)</small></h3>
-                    </div>
-                    <div class="panel-body panel-collapse collapse" id="collapseImage">
-                        <div class="row">
-                            @if( isset( $eform['customer']['other']['identity'] ) )
-                                <div class="col-md-6" align="center">
-                                    <div class="card-box">
-                                        @if((pathinfo(strtolower($eform['customer']['other']['identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($eform['customer']['other']['identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($eform['customer']['other']['identity'])), PATHINFO_EXTENSION) == 'jpeg'))
-                                             @if(strpos($eform['customer']['personal']['couple_identity'], 'noimage.jpg'))
-                                                <p>KTP Tidak Ada</p>
-                                                <img class="img-responsive" id="zoom">
-                                            @else
-                                                <img src="{{$eform['customer']['other']['identity']}}" class="img-responsive" id="zoom">
-                                                <p>KTP</p>
-                                            @endif
-                                        @else
-                                            <a href="@if(!empty($eform['customer']['other']['identity'])){{$eform['customer']['other']['identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
-                                            <p>Klik Untuk Lihat KTP</p>
-                                        @endif
-                                    </div>
-                                </div>
-                            @endif
-                            @if( $eform['customer']['personal']['status_id'] == '2' )
-                                @if( isset( $eform['customer']['personal']['couple_identity'] ) )
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-color panel-primary">
+                        <div class="panel-heading" data-toggle="collapse" href="#collapseImage" style="cursor: pointer;">
+                            <h3 class="panel-title">Dokumen Sumber <small>(klik disini untuk melihat detail dokumen)</small></h3>
+                        </div>
+                        <div class="panel-body panel-collapse collapse" id="collapseImage">
+                            <div class="row">
+                                @if( isset( $eform['customer']['other']['identity'] ) )
                                     <div class="col-md-6" align="center">
                                         <div class="card-box">
-                                            @if((pathinfo(strtolower($eform['customer']['personal']['couple_identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($eform['customer']['personal']['couple_identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($eform['customer']['personal']['couple_identity'])), PATHINFO_EXTENSION) == 'jpeg'))
-                                                @if(strpos($eform['customer']['personal']['couple_identity'], 'noimage.jpg'))
-                                                    <p>KTP Pasangan Tidak Ada</p>
+                                            @if((pathinfo(strtolower($eform['customer']['other']['identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($eform['customer']['other']['identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($eform['customer']['other']['identity'])), PATHINFO_EXTENSION) == 'jpeg'))
+                                                 @if(strpos($eform['customer']['personal']['couple_identity'], 'noimage.jpg'))
+                                                    <p>KTP Tidak Ada</p>
                                                     <img class="img-responsive" id="zoom">
                                                 @else
-                                                    <img src="{{$eform['customer']['personal']['couple_identity']}}" class="img-responsive" id="zoom">
-                                                    <p>KTP Pasangan</p>
+                                                    <img src="{{$eform['customer']['other']['identity']}}" class="img-responsive" id="zoom">
+                                                    <p>KTP</p>
                                                 @endif
                                             @else
-                                                <a href="@if(!empty($eform['customer']['personal']['couple_identity'])){{$eform['customer']['personal']['couple_identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
-                                                <p>Klik Untuk Lihat KTP Pasangan</p>
+                                                <a href="@if(!empty($eform['customer']['other']['identity'])){{$eform['customer']['other']['identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
+                                                <p>Klik Untuk Lihat KTP</p>
                                             @endif
                                         </div>
                                     </div>
                                 @endif
-                            @endif
+                                @if( $eform['customer']['personal']['status_id'] == '2' )
+                                    @if( isset( $eform['customer']['personal']['couple_identity'] ) )
+                                        <div class="col-md-6" align="center">
+                                            <div class="card-box">
+                                                @if((pathinfo(strtolower($eform['customer']['personal']['couple_identity']), PATHINFO_EXTENSION) == 'jpg') || (pathinfo(strtolower($eform['customer']['personal']['couple_identity']), PATHINFO_EXTENSION) == 'png') || (pathinfo((strtolower($eform['customer']['personal']['couple_identity'])), PATHINFO_EXTENSION) == 'jpeg'))
+                                                    @if(strpos($eform['customer']['personal']['couple_identity'], 'noimage.jpg'))
+                                                        <p>KTP Pasangan Tidak Ada</p>
+                                                        <img class="img-responsive" id="zoom">
+                                                    @else
+                                                        <img src="{{$eform['customer']['personal']['couple_identity']}}" class="img-responsive" id="zoom">
+                                                        <p>KTP Pasangan</p>
+                                                    @endif
+                                                @else
+                                                    <a href="@if(!empty($eform['customer']['personal']['couple_identity'])){{$eform['customer']['personal']['couple_identity']}}@endif" target="_blank" class="img-responsive"><img src="{{asset('assets/images/download-logo.png')}}" class="img-responsive"></a>
+                                                    <p>Klik Untuk Lihat KTP Pasangan</p>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
