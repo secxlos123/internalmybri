483997a978d62df71d440b07c005e1c4ea7c48a3
Erwan Akse <erwanakse@gmail.com>
2018-03-20 11:32:23 +0700

    fix recomend ao and data penghasilan recontest

--- resources/views/internals/eform/recontest/_lkn-income-recontest.blade.php
+++ resources/views/internals/eform/recontest/_lkn-income-recontest.blade.php
@@ -5,41 +5,41 @@
     <div class="col-md-6">
         <form class="form-horizontal" role="form">
             <div class="form-group">
-                @if ($detail['visit_report']['source'] == 'fixed')
+                @if ($detail['recontest']['source'] == 'fixed')
                 <label class="col-md-5 control-label">Gaji / THP per-Bulan :</label>
                 <div class="col-md-7">
-                <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_salary'], 2, ",", ".") }}</p>
+                <p class="form-control-static">Rp. {{ number_format($detail['recontest']['income_salary'], 2, ",", ".") }}</p>
                 @else
                 <label class="col-md-5 control-label">Penghasilan per-Bulan :</label>
                 <div class="col-md-7">
-                <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income'], 2, ",", ".") }}</p>
+                <p class="form-control-static">Rp. {{ number_format($detail['recontest']['income'], 2, ",", ".") }}</p>
                 @endif
                 </div>
             </div>
-            @if ($detail['visit_report']['source'] == 'fixed')
+            @if ($detail['recontest']['source'] == 'fixed')
             <div class="form-group">
                 <label class="col-md-5 control-label">Tunjangan / Insentif Lain :</label>
                 <div class="col-md-7">
-                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['income_allowance'], 2, ",", ".") }}</p>
+                    <p class="form-control-static">Rp. {{ number_format($detail['recontest']['income_allowance'], 2, ",", ".") }}</p>
                 </div>
             </div>
             @endif
         </form>
     </div>
-    @if(($detail['visit_report']['source_income'] != 'single') && ($detail['customer']['personal']['status_id'] == 2))
+    @if(($detail['recontest']['source_income'] != 'single') && ($detail['customer']['personal']['status_id'] == 2))
     <div class="col-md-6">
         <form class="form-horizontal" role="form">
             <div class="form-group">
                 <label class="col-md-5 control-label">Penghasilan Pasangan :</label>
                 <div class="col-md-7">
-                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['couple_salary'], 2, ",", ".") }}</p>
+                    <p class="form-control-static">Rp. {{ number_format($detail['recontest']['couple_salary'], 2, ",", ".") }}</p>
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-md-5 control-label">Penghasilan Lain Pasangan :</label>
                 <div class="col-md-7">
-                    <p class="form-control-static">Rp. {{ number_format($detail['visit_report']['couple_other_salary'], 2, ",", ".") }}</p>
+                    <p class="form-control-static">Rp. {{ number_format($detail['recontest']['couple_other_salary'], 2, ",", ".") }}</p>
                 </div>
             </div>
         </form>
