901d4c14174a28bcc50d1ca955f34483987052b2
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-03-22 15:27:23 +0700

    fix starting page

--- resources/views/internals/eform/index.blade.php
+++ resources/views/internals/eform/index.blade.php
@@ -213,7 +213,20 @@
             order : [[3, 'asc']],
             "language": {
                 "emptyTable": "No data available in table"
-            }
+            },
+            [
+                {   data: 'ref_number', name: 'ref_number',  bSortable: false },
+                {   data: 'customer_name', name: 'customer_name',  bSortable: false  },
+                {   data: 'request_amount', name: 'request_amount',  bSortable: false  },
+                {   data: 'created_at', name: 'created_at' },
+                {   data: 'branch_id', name: 'branch_id', bSortable: false, className: 'hidden' },
+                {   data: 'prescreening_status', name: 'prescreening_status', bSortable: false },
+                {   data: 'id', name: 'eforms.id', bSortable: false, className: 'hidden' },
+                {   data: 'ao_name', name: 'ao_name', bSortable: false },
+                {   data: 'status', name: 'status', bSortable: false },
+                {   data: 'aging', name: 'aging' },
+                {   data: 'action', name: 'action', bSortable: false },
+            ]
         });
 
     $(document).on('click', "#btn-filter", function(){
@@ -260,7 +273,6 @@
                 {   data: 'customer_name', name: 'customer_name',  bSortable: false  },
                 {   data: 'request_amount', name: 'request_amount',  bSortable: false  },
                 {   data: 'created_at', name: 'created_at' },
-                // {   data: 'product_type', name: 'product_type' },
                 {   data: 'branch_id', name: 'branch_id', bSortable: false, className: 'hidden' },
                 {   data: 'prescreening_status', name: 'prescreening_status', bSortable: false },
                 {   data: 'id', name: 'eforms.id', bSortable: false, className: 'hidden' },
