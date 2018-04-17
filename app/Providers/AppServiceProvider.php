1a058625e0c65d5d08635c49d0fb426341a13c23
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-04-17 16:19:11 +0700

    create logic web http or https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if( env('IS_SSL', 1) ){
+            \URL::forceScheme('https');
+        }
     }
 
     /**
