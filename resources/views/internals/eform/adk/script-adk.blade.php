9f0c62f616251407fb0908f4255c9e609d4cc244
secxlos <evan.tohap@lawencom.com>
2018-05-22 12:58:59 +0700

    update adk

--- resources/views/internals/eform/adk/script-adk.blade.php
+++ resources/views/internals/eform/adk/script-adk.blade.php
@@ -36,13 +36,6 @@
 
     function reloadData1()
     {
-			var host = <?php echo env('APP_URL');?>;
-		var url = '';
-		if(host ='https://internalmybri.bri.co.id/){
-			url =  '/datatables/adk-list';
-		}else{
-			url =  '/internal/datatables/adk-list';
-		}
         table1 = $('#datatable').DataTable({
             searching : true,
             processing : true,
@@ -56,7 +49,7 @@
                 infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
             },
             ajax : {
-                url : url,
+                url : '/internal/datatables/adk-list';,
                 data : function(d, settings){
                     var api = new $.fn.dataTable.Api(settings);
                     // console.log(settings.json.data);
--- resources/views/internals/eform/adk/script-his-adk.blade.php
+++ resources/views/internals/eform/adk/script-his-adk.blade.php
@@ -1,13 +1,6 @@
 <script type="text/javascript">
 
 	$(document).ready(function(){
-		var host = <?php echo env('APP_URL');?>;
-		var url = '';
-		if(host ='https://internalmybri.bri.co.id/){
-			url =  '/datatables/adk-his-list';
-		}else{
-			url =  '/internal/datatables/adk-his-list';
-		}
 		$('#datatable-histori').DataTable({
             searching : true,
             processing : true,
@@ -21,7 +14,7 @@
                 infoFiltered : '(disaring dari _MAX_ data keseluruhan)'
             },
             ajax : {
-                url : url,
+                url : '/internal/datatables/adk-his-list',
                 data : function(d, settings){
                     var api = new $.fn.dataTable.Api(settings);
 
